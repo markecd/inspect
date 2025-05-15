137c4babd730f7be713ce5e101e5c4cb1aab1331
MarkoCe13 <marko.cesnjaj@student.um.si>
2025-05-16 00:15:34 +0200

    Reinicializacija

---
+++ mobileApp/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+      }}
+    >
+      <Text>Inspect je najboljsi app!</Text>
+    </View>
+  );
+}
