589e6e5b34d2085211e45688b0f0542ee8a8d97c
MarkoCe13 <marko.cesnjaj@student.um.si>
2025-05-16 00:15:34 +0200

    Reinicializacija

--- mobileApp/app/_layout.tsx
+++ mobileApp/app/_layout.tsx
@@ -1,35 +1,5 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { SQLiteDatabase, SQLiteProvider } from 'expo-sqlite';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  const createDbIfNeeded = async (db: SQLiteDatabase) => {
-    console.log("Creating database if needed");
-    await db.execAsync(
-      "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT);"  
-    );
-  };
-
-  return (
-    <SQLiteProvider databaseName='test.db' onInit={createDbIfNeeded}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </SQLiteProvider>
-  );
+  return <Stack />;
 }
