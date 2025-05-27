11d22f8605e0e1cd6fd3899e9d74ac09aeee731d
markecd <muricombe.delic@gmail.com>
2025-05-27 14:12:44 +0200

    Stilski popravek barva screen profil

--- mobileApp/app/profile/index.tsx
+++ mobileApp/app/profile/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text, ScrollView } from 'react-native';
 import ProfileDetails from '@/components/Profile/ProfileDetails';
 import AchievementsDetails from '@/components/Profile/AchievementsDetails';
+import styles from '../../assets/styles/Profile/profile-details.style';
+
 
 export default function ProfileScreen() {
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.profileContentContainer}>
       <ProfileDetails></ProfileDetails>
       <AchievementsDetails></AchievementsDetails>
     </ScrollView>
