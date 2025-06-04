f12c871b340ed9a13f43ec229561beb9c62d4fe6
MarkoCe13 <marko.cesnjaj@student.um.si>
2025-06-05 01:56:53 +0200

    Funkcionalnost člankov + UI

--- webApp/inspectWebApp/src/App.tsx
+++ webApp/inspectWebApp/src/App.tsx
@@ -4,16 +4,18 @@ import './App.css'
 import Login from './pages/Login';
 import Articles from './pages/Articles';
 import AddArticle from './pages/AddArticle';
-//import ObservationMap from './pages/ObservationMap';
+import ObservationMap from './pages/ObservationMap';
+import ArticleDetails from './pages/ArticleDetails';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        {/*<Route path="/" element={<ObservationMap />}/>}*/} 
-        <Route path="/" element={<Login />}/>
+        <Route path="/" element={<ObservationMap />}/> 
+        <Route path="/login" element={<Login />}/>
         <Route path="/articles" element={<Articles />}/>
+        <Route path='/articles/:uid/:id' element={<ArticleDetails />}/>
         <Route path="/add-article" element={<AddArticle />}/>
       </Routes>
     </Router>
