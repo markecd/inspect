55a71664378dd6d3fc4d3548bd03474c5af7660a
MarkoCe13 <marko.cesnjaj@student.um.si>
2025-06-06 17:03:24 +0200

    Navbar + Logout + css popravki

--- mobileApp/tests/e2e/environment.js
+++ mobileApp/tests/e2e/environment.js
@@ -1,5 +1,5 @@
-const { DetoxCircusEnvironment } = require('detox/runners/jest-circus');
-const { execSync } = require('child_process');
+import { DetoxCircusEnvironment } from 'detox/runners/jest-circus';
+import { adapter } from 'detox/runners/jest/adapter';
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
   constructor(config, context) {
@@ -8,12 +8,11 @@ class CustomDetoxEnvironment extends DetoxCircusEnvironment {
 
   async setup() {
     await super.setup();
-   
+    this.handleTestEvent = adapter.handleTestEvent;
   }
 
   async teardown() {
     await super.teardown();
-
   }
 }
 
